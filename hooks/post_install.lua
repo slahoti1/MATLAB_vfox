d0d3b73a90fbe1385ba27c0c1a4cbf5eb098ded5
Shivam Lahoti <slahoti@mathworks.com>
2025-07-17 14:39:11 +0530

    with fixed mpm path

--- hooks/post_install.lua
+++ hooks/post_install.lua
@@ -5,23 +5,17 @@
 function PLUGIN:PostInstall(ctx)
   local sep = package.config:sub(1,1)
   local is_windows = sep == "\\"
-  local mpm_bin = is_windows and "mpm.exe" or "mpm"
 
   local version = ctx.sdkInfo["matlab-vfox"].version
   local install_path = ctx.rootPath
-  --local mpm_path = install_path .. sep .. mpm_bin
-
-  --local products = os.getenv("MATLAB_TOOLBOXES") or "MATLAB"
   local products = "MATLAB"
   products = products:gsub(",", " ")
 
-  -- Use the full path to mpm.exe
-  mpm_path = [[C:\\Users\\slahoti\\mpm.exe]]
+  -- Set mpm path based on OS
+  local mpm_path = is_windows and [[C:\Users\slahoti\mpm.exe]] or "mpm"
 
-  -- Only quote the path, not the entire command
+  -- Build the install command
   local cmd = string.format('%s install --release=%s --destination=%s --products=%s', mpm_path, version, install_path, products)
-  --local cmd = [[C:\Users\slahoti\mpm.exe install --release=R2024b --destination=.version-fox\plugin\matlab-vfox\matlab-install --products=MATLAB]]
-  --local cmd = [[runas /user:Administrator "C:\Users\slahoti\mpm.exe install --release=R2024b --destination=C:\Users\slahoti\.version-fox\plugin\matlab-vfox --products=MATLAB"]]
 
   print("[DEBUG] Running: " .. cmd)
 
@@ -29,5 +23,7 @@ function PLUGIN:PostInstall(ctx)
   if res ~= 0 then
       error("MATLAB install failed")
   end
+
   return true
+
 end
