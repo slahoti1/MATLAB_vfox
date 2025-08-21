061f98ac952372804d0c131b87d981aa0086e7b2
Shivam Lahoti <slahoti@mathworks.com>
2025-08-21 12:10:28 +0530

    dynamically handling installations

--- hooks/env_keys.lua
+++ hooks/env_keys.lua
@@ -3,24 +3,7 @@
 --- Note: Be sure to distinguish between environment variable settings for different platforms!
 --- @param ctx table Context information
 --- @field ctx.path string SDK installation directory
---[[function PLUGIN:EnvKeys(ctx)
-  local mainPath = ctx.path
 
-  -- On Windows, use ';', on Unix, use ':'
-  local sep = package.config:sub(1,1) == '\\' and ';' or ':'
-
-  return {
-      {
-          key = "PATH",
-          value = mainPath .. "\\bin" .. sep .. os.getenv("PATH")
-      }
-      -- You can add more variables like this:
-      -- {
-      --     key = "MATLAB_HOME",
-      --     value = mainPath
-      -- }
-  }
-end]]
 function PLUGIN:EnvKeys(ctx)
   --- this variable is same as ctx.sdkInfo['plugin-name'].path
   local mainPath = ctx.path
@@ -30,16 +13,87 @@ function PLUGIN:EnvKeys(ctx)
   local path = sdkInfo.path
   local version = sdkInfo.version
   local name = sdkInfo.name
-  return {
-    {
-      key = "PATH",
-      value = mainPath
-    },
-  
-    {
-      key = "PATH",
-      value = mainPath .. "\\bin"
-    }
 
+-----------------------
+----custom directory
+
+ -- Get toolboxes from environment variable
+ local toolboxes_env = os.getenv("MATLAB_TOOLBOXES")
+
+ if toolboxes_env then
+  print("[DEBUG] MATLAB_TOOLBOXES env variable is set to: '" .. toolboxes_env .. "'")
+  io.stderr:write("[DEBUG] MATLAB_TOOLBOXES env variable is set to: '" .. toolboxes_env .. "'\n")
+else
+  print("[DEBUG] MATLAB_TOOLBOXES env variable is NOT set or is nil")
+  io.stderr:write("[DEBUG] MATLAB_TOOLBOXES env variable is NOT set or is nil\n")
+end
+
+
+
+ local products = "MATLAB"
+ if toolboxes_env and #toolboxes_env > 0 then
+   products = products .. "," .. toolboxes_env
+ end
+ products = products:gsub(",", " ")
+
+
+local install_path = mainPath
+local folder_name
+if not toolboxes_env or #toolboxes_env == 0 then
+  folder_name = "myMATLAB_" .. version
+elseif toolboxes_env == "Simulink" then
+  folder_name = "mySimulink_" .. version
+else
+  folder_name = "myOtherToolboxes_" .. version
+end
+
+-- Get parent directory of install_path
+local sep = package.config:sub(1,1)
+local parent_dir = install_path:match("^(.*" .. sep .. ")") or "."
+
+-- Remove trailing separator if present
+if parent_dir:sub(-1) == sep then
+  parent_dir = parent_dir:sub(1, -2)
+end
+
+-- Concatenate parent directory and new folder name
+local install_dir = parent_dir .. sep .. folder_name
+
+-- Function to check if directory exists
+local function file_exists(path)
+  local f = io.open(path, "r")
+  if f then f:close(); return true end
+  return false
+end
+
+local function install_exists(install_dir)
+  local sep = package.config:sub(1,1)
+  local marker_file = install_dir .. sep .. "//bin//matlab.exe"  -- Or another marker file
+  return file_exists(marker_file)
+end
+
+if not install_exists(install_dir) then
+  io.stderr:write("WARNING: Required installation not present at " .. install_dir .. "\n")
+  -- Return an empty table so mise/vfox does not error
+  --return {}
+  mainPath = install_dir
+else
+  -- Add to PATH
+ mainPath = install_dir
+-----------------------
+end
+
+return {
+  {
+    key = "PATH",
+    value = mainPath
+  },
+
+  {
+    key = "PATH",
+    value = mainPath .. "\\bin"
   }
+
+}
+
 end
--- hooks/post_install.lua
+++ hooks/post_install.lua
@@ -3,6 +3,7 @@
 --- Currently can be left unimplemented!
 
 function PLUGIN:PostInstall(ctx)
+  
   local sep = package.config:sub(1,1)
   local is_windows = sep == "\\"
 
@@ -17,12 +18,86 @@ function PLUGIN:PostInstall(ctx)
   end
   products = products:gsub(",", " ")
 
+-------------------------------
+----trying the hashing
+
+
+---- getting the directory and installation in there
+
+local folder_name
+if not toolboxes_env or #toolboxes_env == 0 then
+  folder_name = "myMATLAB_" .. version
+elseif toolboxes_env == "Simulink" then
+  folder_name = "mySimulink_" .. version
+else
+  folder_name = "myOtherToolboxes_" .. version
+end
+
+-- Get parent directory of install_path
+local sep = package.config:sub(1,1)
+local parent_dir = install_path:match("^(.*" .. sep .. ")") or "."
+
+-- Remove trailing separator if present
+if parent_dir:sub(-1) == sep then
+  parent_dir = parent_dir:sub(1, -2)
+end
+
+-- Concatenate parent directory and new folder name
+local install_dir = parent_dir .. sep .. folder_name
+
+-- Function to check if directory exists
+local function file_exists(path)
+  local f = io.open(path, "r")
+  if f then f:close(); return true end
+  return false
+end
+
+-----------remove a directory
+local function remove_dir(dir)
+  local sep = package.config:sub(1,1)
+  local cmd
+  if sep == "\\" then
+    -- Windows
+    cmd = string.format('rmdir /s /q "%s"', dir)
+  else
+    -- Unix/Linux/Mac
+    cmd = string.format('rm -rf "%s"', dir)
+  end
+  os.execute(cmd)
+end
+
+
+local function install_exists(install_dir)
+  local sep = package.config:sub(1,1)
+  local marker_file = install_dir .. sep .. "//bin//matlab.exe"  -- Or another marker file
+  return file_exists(marker_file)
+end
+
+if not install_exists(install_dir) then
+  print("Creating install directory: " .. install_dir)
+  -- Cross-platform mkdir -p
+  local is_windows = package.config:sub(1,1) == "\\"
+  local mkdir_cmd
+  if is_windows then
+    mkdir_cmd = 'mkdir "' .. install_dir .. '"'
+  else
+    mkdir_cmd = 'mkdir -p "' .. install_dir .. '"'
+  end
+  assert(os.execute(mkdir_cmd), "Failed to create directory: " .. install_dir)
+
+  -- Do your installation here
+  print("Installing toolbox in " .. install_dir)
+  -- ...installation logic...
+
+
+-----------------------------
+
   -- Set mpm path based on OS
   --local mpm_path = is_windows and [[C:\Users\slahoti\mpm.exe]] or "mpm"
   local mpm_path = "mpm"
 
   -- Build the install command
-  local cmd = string.format('%s install --release=%s --destination=%s --products=%s', mpm_path, version, install_path, products)
+  local cmd = string.format('%s install --release=%s --destination=%s --products=%s', mpm_path, version, install_dir, products)
 
   print("[DEBUG] Running: " .. cmd)
 
@@ -30,6 +105,12 @@ function PLUGIN:PostInstall(ctx)
   if res ~= 0 then
       error("MATLAB install failed")
   end
+
+else
+  print("Installation already exists at " .. install_dir .. ". Skipping install.")
+end
+
+remove_dir(parent_dir .. sep .. version)
 
   return true
 end
